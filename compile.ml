0286104b01f9a6c917ef17dfd5e6094254a932a0
William Diebolt <wmdiebolt@gmail.com>
2023-09-29 14:18:43 -0700

    let and binary op works. 3 programs compile and run. need to implement single arg ops in compile.ml

--- compile.ml
+++ compile.ml
@@ -19,21 +19,41 @@ let rec compile_expr (e : expr) (si : int) (env : (string * int) list) : instruc
 	let bis = compile_expr body (si + List.length binding) ext_env in
 	vis @ bis
   | ENumber(i) -> [IMov(Reg RAX, Const i)]
-  | EId(x) -> (match find env x with
-        | None -> failwith "Unbound id"
-        | Some(i) -> [IMov(Reg RAX, stackloc i)])
+  | EId(x) -> (
+    match find env x with
+    | None -> failwith "Unbound id"
+    | Some(i) -> [IMov(Reg RAX, stackloc i)] )
 
+(* Tail Recursive implementation needs to *reverse* the instruction list as usual trick. In this case, it is a little tricky because the list, ins, is built hierarchically in sections and subsections which must remain ordered *)
 and compile_binding b si env = 
-  (* TODO *)
-  failwith "Not yet implemented"
+  let rec iter b si env ins =
+    match b with
+    | [] -> (List.rev ins, env)
+    | (x,v)::more -> (
+      let vis = compile_expr v si env in 
+      let sis = [IMov(stackloc si, Reg RAX)] in
+      iter more (si+1) ((x,si)::env) (sis @ (List.rev vis) @ ins) ) 
+  in iter b si env []
 
 and compile_prim1 op e si env =
   (* TODO *)
   failwith "Not yet implemented"
 
 and compile_prim2 op e1 e2 si env =
-  (* TODO *)
-  failwith "Not yet implemented"
+  let e1is = compile_expr e1 si env in
+  let e2is = compile_expr e2 (si + 1) env in
+  let op_is = (
+    match op with
+    | Plus ->  [IAdd(Reg RAX, stackloc (si + 1))]
+ 	| Minus -> [ISub(Reg RAX, stackloc (si + 1))]
+	| Times -> [ISub(Reg RAX, stackloc (si + 1))] )
+  in
+    e1is @
+    [IMov(stackloc si, Reg RAX)] @
+    e2is @
+    [IMov(stackloc (si+1), Reg RAX)] @
+    [IMov(Reg RAX, stackloc si)] @
+    op_is
 
 let compile_to_string prog =
   let prelude =
